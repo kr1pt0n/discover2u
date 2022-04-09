ce92d09ab854a8b6bba562742dddffd6e217001e
kr1pt0n <39812156+kr1pt0n@users.noreply.github.com>
2022-04-08 22:48:03 -0500

    Update d2u.sh

--- d2u.sh
+++ d2u.sh
@@ -16,7 +16,8 @@ tput civis
 for i in $(seq 1 254); do
 
 #Cambiar el rango de direccion ip por la que desea escanear.
-#Se envia una traza icmp al equipo donde devolvera un codigo de estado 1 o 0, donde 0 es correcto y 1 negativo.
+#Se envia una traza icmp a distintos hosts del rango de ip ( 1-254)
+#qe devolvera un codigo de estado 1 o 0, y procedera con la siguiente instruccion, dependiendo de ello.
 
 	timeout 1 bash -c "ping -c 1 192.168.1.$i" &>/dev/null && echo "[+] Host 192.168.1.$i - ACTIVE" &
 
