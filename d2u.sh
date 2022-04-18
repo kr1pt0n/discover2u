05a1af2adf1086d78d4842647fadeabcf82fb507
kr1pt0n <39812156+kr1pt0n@users.noreply.github.com>
2022-04-18 11:07:13 -0500

    Update d2u.sh

--- d2u.sh
+++ d2u.sh
@@ -15,9 +15,7 @@ tput civis
 
 for i in $(seq 1 254); do
 
-#Cambiar el rango de direccion ip por la que desea escanear.
-#Se envia una traza icmp a distintos hosts del rango de ip ( 1-254)
-#qe devolvera un codigo de estado 1 o 0, y procedera con la siguiente instruccion, dependiendo de ello.
+#Cambiar el segmento de red "192.168.1", por la que desea escanear.
 
 	timeout 1 bash -c "ping -c 1 192.168.1.$i" &>/dev/null && echo "[+] Host 192.168.1.$i - ACTIVE" &
 
